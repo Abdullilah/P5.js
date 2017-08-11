752b5a8a537c2effb3259ae2021b50d879399eb9
Abdullilah <abdullilaphd@hotmail.com>
2017-08-11 19:24:06 +0200

    boolean values and balls game

--- sketch.js
+++ sketch.js
@@ -1,31 +1,40 @@
-var pos = {
-    x: 0,
-    y: 0
+var circle1 = {
+    xPos: 50,
+    yPos: 200,
+    speed: 10
 }
-
-var col = {
-    r: 0,
-    g: 0,
-    b: 0
+var circle2 = {
+    xPos: 400,
+    yPos: 50,
+    speed: 10
 }
 function setup() {
     createCanvas(800,400);
-    background(0,0,0);
 }
 
 function draw() {
     
-    pos.x = random(0, 800); // random(minRange,maxRange)
-    pos.y = random(0, 400);
+    background(0,0,0);
     
-    col.r = random(0, 255);
-    col.g = random(0, 255);
-    col.b = random(0, 255);
+    stroke(255); 
+    strokeWeight(4);
     
-    noStroke(); 
-    fill(col.r, col.g , col.b, 100);
-    ellipse(pos.x,pos.y, 20,20); 
-    
+    if(circle1.xPos > width-50 || circle1.xPos < 50){
+        circle1.speed = circle1.speed* -1;
+    } 
+    circle1.xPos += circle1.speed;
+    ellipse(circle1.xPos, circle1.yPos, 100,100); 
+    fill(250, 0 , 0);
+
+    if(circle2.yPos > height-50 || circle2.yPos < 50){
+        circle2.speed = circle2.speed* -1;
+    } 
+    circle2.yPos += circle2.speed;
+    ellipse(circle2.xPos, circle2.yPos, 100,100); 
+    fill(0, 250 , 0);
 }
 
-
+function mousePressed(){
+    circle1.xPos = 50;
+    circle2.yPos = 50;
+}
