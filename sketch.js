67f3ae3a75c246177edfcdeb9ed6e51bfcd03acc
Abdullilah <abdullilaphd@hotmail.com>
2017-08-11 17:47:21 +0200

    map function

--- sketch.js
+++ sketch.js
@@ -1,39 +1,18 @@
-var xLocation = 0; // declear variable
-var myCircle = {
-    x: 500,
-    y: 300,
-    size: 20
-}
-
+var col = 0;
 function setup() {
-    createCanvas(500,500);
-    background(100,100,100);
+    createCanvas(600,200);
 }
 
 function draw() {
-    fill(10,10,10);
-    rect(10,10,100,100);
-    
-    noStroke();
-    fill(255,0,0);
-    ellipse(240,115, 60,60);
-    
-    line(200,200,300,300);
-    
-    noStroke(); // remove borders
+    //col = (255*mouseX)/600;
+    //Using map() to make the same:
+    //map(parameter, minOldrange, maxOldrange, minNewrange, maxNewrange);
+    col = map(mouseX, 0, 600, 0, 255);
+    background(col);
+    noStroke(); 
     fill(255,255,0);
-    ellipse(mouseX,mouseY, 10,10); // mouse position
+    ellipse(mouseX,mouseY, 10,10); 
     
-    fill(0,0,250);
-    rect(xLocation,200,50,50);
-    xLocation += 1;
-    
-    noStroke();
-    fill(0,255,0);
-    ellipse(myCircle.x, myCircle.y, myCircle.size, myCircle.size);
-    myCircle.x -= 1;
 }
 
-function mousePressed(){
-    background(100,100,100); // mouse click event
-}
+
