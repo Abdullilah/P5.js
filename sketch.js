ae4f5f80da07b613d81dd3186b5c78410186613f
Abdullilah <abdullilaphd@hotmail.com>
2017-08-11 22:17:24 +0200

    X and Y change direction

--- sketch.js
+++ sketch.js
@@ -1,3 +1,9 @@
+var ball = {
+    x: 10,
+    y: 10,
+    speedX: 10,
+    speedY: 10
+}
 function setup() {
   createCanvas(800,500);
   
@@ -7,12 +13,28 @@ function draw() {
     background(0,0,0);
     stroke(255); 
     strokeWeight(1);
+    
+    fillColor();
+    move();
+    checkIt();
+}
 
-    for (var x=25; x<= mouseX; x+= 50){
-        for (var y=25; y<= mouseY; y+= 50){
-            fill(random(0,255),0,random(0,255));
-            ellipse(x,y,25,25);
-        }
+function fillColor(){
+    ellipse(ball.x,ball.y,20,20);
+    fill(random(0,255),0,random(0,255));
+}
+
+function move(){
+    if(ball.x > width-10 || ball.x < 10){
+        ball.speedX *= -1;
     }
+    if(ball.y > height-10 || ball.y< 10){
+        ball.speedY *= -1;
+    }
+}
+
+function checkIt(){
+    ball.x += ball.speedX;
+    ball.y += ball.speedY;
 }
 
