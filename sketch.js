48970c833ce7768a60a3f60656138d3be819de83
Abdullilah <abdullilaphd@hotmail.com>
2017-08-13 17:10:27 +0200

    Use function multi times

--- sketch.js
+++ sketch.js
@@ -1,9 +1,23 @@
-var ball = {
-    x: 10,
-    y: 10,
-    speedX: 10,
-    speedY: 10
+var ball1 = {
+    x: 100,
+    y: 100,
+    size: 100,
+    stickx:80,
+    sticky: 140,
+    stickWidth: 40,
+    stickHeight: 100
 }
+
+var ball2 = {
+    x: 300,
+    y: 200,
+    size: 150,
+    stickx:280,
+    sticky: 240,
+    stickWidth: 40,
+    stickHeight: 100
+}
+
 function setup() {
   createCanvas(800,500);
   
@@ -14,27 +28,23 @@ function draw() {
     stroke(255); 
     strokeWeight(1);
     
-    fillColor();
-    move();
-    checkIt();
+    stickPlot(ball1.stickx,ball1.sticky,ball1.stickWidth,ball1.stickHeight);
+    ballPolat(ball1.x,ball1.y,ball1.size,ball1.size);
+    
+    stickPlot(ball2.stickx,ball2.sticky,ball2.stickWidth,ball2.stickHeight);
+    ballPolat(ball2.x,ball2.y,ball2.size,ball2.size);
 }
 
-function fillColor(){
-    ellipse(ball.x,ball.y,20,20);
-    fill(random(0,255),0,random(0,255));
+function stickPlot(x,y,z,m){
+    rect(x,y,z,m);
+    fill(0,0,255);
 }
 
-function move(){
-    if(ball.x > width-10 || ball.x < 10){
-        ball.speedX *= -1;
-    }
-    if(ball.y > height-10 || ball.y< 10){
-        ball.speedY *= -1;
-    }
+function ballPolat(x,y,z,m){
+    ellipse(x,y,z,m);
+    fill(255,0,0);
 }
 
-function checkIt(){
-    ball.x += ball.speedX;
-    ball.y += ball.speedY;
-}
+
+
 
