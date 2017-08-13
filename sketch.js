71d87ddb6ae1c28cd95fc1eb14126f8cd08ae0b8
Abdullilah <abdullilaphd@hotmail.com>
2017-08-14 00:40:24 +0200

    Click on object

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 var balls = [];
 var numberBalls = 10;
+var col;
 
 function setup() {
     createCanvas(800,500);
+    for(var i=0; i<numberBalls; i++){
+        balls[i] = new Ball();
+    }
 }
 
 function draw() {
@@ -12,33 +16,32 @@ function draw() {
         balls[i].display();
         balls[i].move();
     }
-    
 }
 
-function mouseDragged(){
-    balls.push(new Ball(mouseX, mouseY));
-    if(balls.length > 20){
-        balls.splice(0,1);
+function mousePressed(){
+    for(var i=0; i<balls.length; i++){
+        balls[i].clicked();
     }
-}
-
-function keyPressed(){
-    balls.splice(0,1);
 }
 
 //Constructor: start with big letter
 function Ball(x,y){
-    this.x = x;
-    this.y = y;
+    this.x = random(0,width);
+    this.y = random(0,height);
+    this.col = color(255,255,255);
     this.display = function(){
-        stroke(255);
-        fill(random(0,255),random(0,255),random(0,255),100);
-        strokeWeight(2);
-        ellipse(this.x,this.y,25,25);
+        fill(this.col);
+        ellipse(this.x,this.y,40,40);
     }
     this.move = function(){
-        this.x = this.x + random(-2,2);
-        this.y = this.y + random(-2,2);
+        this.x = this.x + random(-1,1);
+        this.y = this.y + random(-1,1);
+    }
+    this.clicked = function(){
+        var d = dist(mouseX, mouseY, this.x, this.y);
+        if(d< 20){
+            this.col = color(255,0,0);
+        }
     }
 }
 
