afb0e9624f2265e44aa0a3719d40a6020a659013
Abdullilah <abdullilaphd@hotmail.com>
2017-08-13 19:00:25 +0200

    arrays

--- sketch.js
+++ sketch.js
@@ -1,33 +1,23 @@
-var ball = {
-    x: 400,
-    y: 400,
-    size: 40,
-    display: function(){
-        rect(this.x,this.y,this.size,this.size);
-        fill(0,0,255);
-    },
-    move: function(){
-        this.x = this.x + random(-40,40);
-        this.y = this.y + random(-40,40);
-        if(this.x<0 || this.x > width || this.y<0 || this.y> height){
-            this.x = 400;
-            this.y = 400;
-        }
-    }
-}
-
+var colors = [255,0,0,0,255,0,0,0,255];
+var index = 0;
 function setup() {
   createCanvas(800,500);
-  
+  background(0);
 }
 
 function draw() {
-    background(0,0,0);
-    stroke(255); 
-    strokeWeight(1);
-    
-    ball.display();
-    ball.move();
+    textSize(32);
+    fill(100,100,100);
+    text('Change color By clicking', width/4, height/2);
+}
+
+function mousePressed(){
+    background(colors[index],colors[index+1],colors[index+2]);
+    index += 3;
+    if(index >= colors.length){
+        index = 0;
+    }
+    print(colors[index]);
 }
 
 
