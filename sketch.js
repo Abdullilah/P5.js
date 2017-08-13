906f4d421b7757ccb21fd114c6f58b5c4198f5ab
Abdullilah <abdullilaphd@hotmail.com>
2017-08-13 23:07:05 +0200

    Constractor

--- sketch.js
+++ sketch.js
@@ -8,20 +8,7 @@ function setup() {
 function draw() {
     background(0);
     for(var i=0; i<numberBalls; i++){
-        balls[i] = {
-            x: random(0,width),
-            y: random(0,height),
-            display: function(){
-                stroke(255);
-                noFill();
-                strokeWeight(2);
-                ellipse(this.x,this.y,25,25);
-            },
-            move: function(){
-                this.x = this.x + random(-5,5);
-                this.y = this.y + random(-5,5);
-            }
-        }
+        balls[i] = new Ball();
     }
     for(var i=0; i<balls.length; i++){
         balls[i].display();
@@ -33,6 +20,20 @@ function mousePressed(){
     numberBalls += 10;
 }
 
-
+//Constructor: start with big letter
+function Ball(){
+    this.x = random(0,width);
+    this.y = random(0,height);
+    this.display = function(){
+        stroke(255);
+        noFill();
+        strokeWeight(2);
+        ellipse(this.x,this.y,25,25);
+    }
+    this.move = function(){
+        this.x = this.x + random(-5,5);
+        this.y = this.y + random(-5,5);
+    }
+}
 
 
