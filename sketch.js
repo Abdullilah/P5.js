46683f7f94bdf767ba7ae97a00248dd0c8887572
Abdullilah <abdullilaphd@hotmail.com>
2017-08-18 03:01:41 +0200

    drag, drop, createImg

--- sketch.js
+++ sketch.js
@@ -1,20 +1,27 @@
-var myArr = ["Sami", "David", "Monika", "Samolia"];
-
 function setup() {
     
-    for(var i=0; i<10; i++){
-        var randVar = floor(random(0,myArr.length));
-        var li = createElement('li', myArr[randVar]);
-        li.class('green');
-        li.parent('myList');
-        li.mousePressed(myFun);
-    }
+    var dragArea = select('.dragHere');
+    
+    dragArea.dragOver(highLight);
+    dragArea.dragLeave(unhighLight);
+    dragArea.drop(myFile, unhighLight);
     
 }
 
-function myFun(){
-    this.removeClass('green');
-    this.class('blue');
+function highLight(){
+    this.style('background', '#ccc');
+}
+
+function unhighLight(){
+    this.style('background', '#fff');
+}
+
+function myFile(file){
+    createP(file.name);
+    createP(file.size);
+    createP(file.type);
+    var img = createImg(file.data);
+    img.size(200,200);
 }
 
 function draw() {
